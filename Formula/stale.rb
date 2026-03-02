52107e4f09879337d7d61319174df7ae2d13434b
th1nkful <9315568+th1nkful@users.noreply.github.com>
2026-03-02 22:06:33 +0000

    Update Homebrew formula for 0.2.0

--- Formula/stale.rb
+++ Formula/stale.rb
@@ -1,9 +1,9 @@
 class Stale < Formula
   desc "CLI tool to run or skip commands based on file content hashes"
   homepage "https://github.com/th1nkful/stale"
-  url "https://github.com/th1nkful/stale/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "a0894b54d9c30ca148a6cb9798ee9d78baf8e90127cd3c33aacd442f235db4f0"
-  version "0.1.0"
+  url "https://github.com/th1nkful/stale/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "e571ecb91adfa3955a403c91cfbba4e3a19041cfc9697f3ed187b09190c4e798"
+  version "0.2.0"
   license "Apache-2.0"
 
   depends_on "rust" => :build
